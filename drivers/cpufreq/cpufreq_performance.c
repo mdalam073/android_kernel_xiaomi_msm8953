8e615a36f2d287d99f8f548798364a200b951a8d
Amit Kucheria <amit.kucheria@linaro.org>
2019-07-08 15:28:33 +0530

    drivers: cpufreq: Initialize governors earlier
    Initialize cpufreq governors earlier so we can run at the highest
    frequencies sooner during boot (in case the bootloader programmed a
    conservative frequency) and conversely throttle back earlier too in case
    thermal mitigation is needed.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- drivers/cpufreq/cpufreq_performance.c
+++ drivers/cpufreq/cpufreq_performance.c
@@ -48,5 +48,10 @@ MODULE_AUTHOR("Dominik Brodowski <linux@brodo.de>");
 MODULE_DESCRIPTION("CPUfreq policy governor 'performance'");
 MODULE_LICENSE("GPL");
 
+<<<<<<< HEAD
 cpufreq_governor_init(cpufreq_gov_performance);
 cpufreq_governor_exit(cpufreq_gov_performance);
+=======
+core_initcall(cpufreq_gov_performance_init);
+module_exit(cpufreq_gov_performance_exit);
+>>>>>>> 52ddcfdfd023 (drivers: cpufreq: Initialize governors earlier)
